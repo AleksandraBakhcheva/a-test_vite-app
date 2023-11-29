07a645a90a28c612a56c503a7ff1a793decb3696
Aleksandra <alexandra.bakhcheva@gmail.com>
2023-11-29 23:51:27 +0300

    update App

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,16 @@
+import "./assets/styles/General.scss";
+import Header from "./assets/components/Header/Header";
+import Main from "./assets/components/Main/Main";
+import Footer from "./assets/components/Footer/Footer";
+
 function App() {
-  return <></>;
+  return (
+    <div className="App">
+      <Header />
+      <Main />
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
