9b2db72552fbdf3dfba91f4be2e6ef17c972a423
Aleksandra <alexandra.bakhcheva@gmail.com>
2023-11-29 23:50:22 +0300

    add main component

---
+++ src/assets/components/Main/Main.jsx
@@ -0,0 +1,73 @@
+import "./Main.scss";
+import Button from "../Button/Button";
+
+export default function Main() {
+  return (
+    <main>
+      <div className="main">
+        <div className="red-light"></div>
+        <div className="red-ball"></div>
+        <div className="main__left-side">
+          <p className="main__left-side__first-header">
+            Зарабатывайте
+            <br />
+            больше
+          </p>
+          <p className="main__left-side__second-header">c WELBEX</p>
+          <p className="main__left-side__text">
+            Развиваем и контролируем
+            <br />
+            продажи за вас
+          </p>
+        </div>
+        <div className="main__right-side">
+          <p className="main__right-side__header">
+            Вместе с&nbsp;
+            <span>
+              бесплатной
+              <br />
+              консультацией
+            </span>
+            &nbsp;мы дарим:
+          </p>
+          <div className="main__right-side__boxes">
+            <div>
+              <h5>Виджеты</h5>
+              <p>
+                30 готовых
+                <br />
+                решений
+              </p>
+            </div>
+            <div>
+              <h5>Dashboard</h5>
+              <p>
+                с показателями
+                <br />
+                вашего бизнеса
+              </p>
+            </div>
+            <div>
+              <h5>Skype Аудит</h5>
+              <p>
+                отдела продаж
+                <br />и CRM системы
+              </p>
+            </div>
+            <div>
+              <h5>35 дней</h5>
+              <p>
+                использования
+                <br />
+                CRM
+              </p>
+            </div>
+          </div>
+          <div className="main__right-side__button">
+            <Button />
+          </div>
+        </div>
+      </div>
+    </main>
+  );
+}
